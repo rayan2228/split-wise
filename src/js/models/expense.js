fef825a33bef980cba7af14f1749c8affaf82bdc
rayan2228 <rayanh.dev@gmail.com>
2026-02-03 19:16:10 +0600

    feat: expense model

---
+++ src/js/models/expense.js
@@ -0,0 +1,20 @@
+class Expense {
+  constructor(paidBy, amount, description = "no description") {
+    if (!paidBy.trim() || typeof paidBy !== "string") {
+      throw new Error("paid by  must be a string type");
+    }
+    if (!amount.trim() || typeof amount !== "number" || amount <= 0) {
+      throw new Error("amount  must be a number type");
+    }
+
+    this.paidBy = paidBy;
+    this.amount = amount;
+    this.description = description;
+    this.date = new Date().toISOString();
+    this.id = this.generateId();
+  }
+
+  generateId() {
+    return crypto.randomUUID();
+  }
+}
